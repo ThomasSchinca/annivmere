1f88fc20f27f9742bd5b7b601982f3aea65acab7
ThomasSchinca <48996514+ThomasSchinca@users.noreply.github.com>
2023-08-05 01:13:37 +0100

    up

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ external_stylesheets=[dbc.themes.LUX]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = 'Anniversaire Mère' 
 app._favicon = ("icone.ico")
+server = app.server
 
 num=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16]
 random.shuffle(num)
